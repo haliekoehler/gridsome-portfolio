e4f3bf4e93914815cb30dba45a82a1cbc42f313f
Halie <halie.koehler@cyberfortress.com>
2020-12-03 14:42:19 -0600

    <g-link> replaces <a> nav links

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,31 +1,23 @@
 <template>
 	<div class="container-fluid">
-		<nav class="navbar navbar-expand-lg sticky-top">
-			<a class="navbar-brand" href="/">halie.online</a>
+		<nav class="navbar navbar-expand-lg navbar-light sticky-top">
+			<!-- <a class="navbar-brand" href="/">halie.online</a> -->
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
-			<div class="collapse navbar-collapse d-flex justify-content-end" id="navbarContent">
+			<div class="collapse navbar-collapse" id="navbarContent">
 				<ul class="navbar-nav">
 					<li class="nav-item">
-						<a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-					</li>
-					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" href="/portfolio" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-							Portfolio
-						</a>
-						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-							<a class="dropdown-item" href="#">Web Design</a>
-							<a class="dropdown-item" href="#">Ux Demo Videos</a>
-							<div class="dropdown-divider"></div>
-							<a class="dropdown-item" href="#">Fine Art</a>
-						</div>
+						<g-link to="/" class="nav-link" exact>Home</g-link>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#">Resume</a>
+						<g-link to="/portfolio/" class="nav-link">Portfolio</g-link>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#">Contact</a>
+						<g-link to="~/assets/resume.pdf" class="nav-link" target="_blank">Resume</g-link>
+					</li>
+					<li class="nav-item">
+						<g-link to="/" class="nav-link">Contact</g-link>
 					</li>
 				</ul>
 			</div>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <App>
-    <div id="main-hero" class="container-fluid">
+    <div id="main-hero" class="container-fluid py-4">
       <h1 class="title">Hello! I'm Halie Koehler.</h1>
       <h2 class="subtitle">UI/UX Designer / Frontend Developer / Visual Artist</h2>
     </div>
